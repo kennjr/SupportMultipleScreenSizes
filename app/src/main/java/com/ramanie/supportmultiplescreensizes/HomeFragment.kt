b2970e1a7848ac0997da16475a5214618a8ff9a3
kenedy <kenedy@myhechar.pro>
2022-11-08 22:43:39 +0300

    wrote the code for the content that;ll be displayed and ctrl-flow for how to display the content

--- app/src/main/java/com/ramanie/supportmultiplescreensizes/HomeFragment.kt
+++ app/src/main/java/com/ramanie/supportmultiplescreensizes/HomeFragment.kt
@@ -1,9 +1,7 @@
 package com.ramanie.supportmultiplescreensizes
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,16 +12,57 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeFragment(){
-    LazyColumn(modifier = Modifier.fillMaxSize()){
-        // first list
-        items(10){
-            Text(text = "Item $it", fontSize = 16.sp, modifier = Modifier.fillMaxWidth().background(
-                Color.LightGray).padding(5.dp))
-        }
-        // second list
-        items(10){
-            Text(text = "Item $it", fontSize = 16.sp, modifier = Modifier.fillMaxWidth().background(
-                Color.DarkGray).padding(5.dp))
+    // we aren't putting this in a remember{} simply bc we need it to get updated whenever the composable is recomposed
+    val windowInfo = rememberWindowInfo()
+    // we're checking if the width of the screen is the smallest width, if that's the case then we're gonna stack the lists
+    if (windowInfo.screenWidthInfo is WindowInfo.WindowType.Compact){
+        LazyColumn(modifier = Modifier.fillMaxSize()){
+            // first list
+            items(10){
+                Text(text = "Item $it", fontSize = 16.sp, modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        Color.LightGray
+                    )
+                    .padding(5.dp))
+            }
+            // second list
+            items(10){
+                Text(text = "Item $it", fontSize = 16.sp, modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        Color.DarkGray
+                    )
+                    .padding(5.dp))
+            }
         }
     }
+    // if the screen's width is greater than 600dp then we wanna place the lists side-by-side
+    else{
+        Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxSize()) {
+            LazyColumn(modifier = Modifier.weight(1f).fillMaxHeight()){
+                // first list
+                items(10){
+                    Text(text = "Item $it", fontSize = 16.sp, modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            Color.LightGray
+                        )
+                        .padding(5.dp))
+                }
+            }
+            LazyColumn(modifier = Modifier.weight(1f).fillMaxHeight()){
+                // second list
+                items(10){
+                    Text(text = "Item $it", fontSize = 16.sp, modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            Color.DarkGray
+                        )
+                        .padding(5.dp))
+                }
+            }
+        }
+    }
+
 }
